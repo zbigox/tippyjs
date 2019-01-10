71b64c7037976cfd3a87da26cdceedb8e11f2724
atomiks <cc.glows@gmail.com>
2019-01-10 22:29:03 +1100

    Updates

---
+++ component---src-pages-404-js-fd5ea0b058136097d288.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{156:function(e,t,n){"use strict";n.r(t);var o=n(10),a=n.n(o),r=n(0),u=n.n(r),l=n(174),p=n(186);t.default=function(e){var t=e.pageContext,n=a()({},t,{frontmatter:{title:"404: Not Found"}});return u.a.createElement(l.a,{pageContext:n},u.a.createElement(p.a,{title:"404: Not found",pageContext:n}),u.a.createElement("p",null,"Unfortunately, the page you were looking for does not exist. 🥵"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fd5ea0b058136097d288.js.map
